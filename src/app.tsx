e1cb58f2dd9a12e31e733d2e41d36a485f0e9b94
gurkanmaral <gurkan.maral@eczacibasi.com.tr>
2025-11-12 21:14:56 +0300

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import Navbar from "./components/Navbar.tsx";
+import Hero from "./components/Hero.tsx";
 
 const App = () => {
     return (
         <main>
             <Navbar />
+            <Hero />
         </main>
     )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useRef} from 'react'
+
+const Hero = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+       if(videoRef.current) {
+           videoRef.current.playbackRate = 0.8
+       }
+    },[])
+    return (
+        <section id="hero">
+            <h1>
+                Macbook Pro
+            </h1>
+            <img
+            src="/title.png"
+            alt="macbook"
+            />
+            <video
+                ref={videoRef}
+                src={"/videos/hero.mp4"} autoPlay={true} muted playsInline />
+
+            <button>
+                Buy
+            </button>
+            <p>
+                From $1599 or $133/mo for 12 months
+            </p>
+        </section>
+    )
+}
+export default Hero
